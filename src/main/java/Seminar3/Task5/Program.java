9e0d60129e29ce0262e88d4c409d5105647e181d
Andrei Shim <art.shim@yandex.ru>
2023-10-25 18:10:10 +0300

    minor changes

--- src/main/java/Seminar3/Task5/Program.java
+++ src/main/java/Seminar3/Task5/Program.java
@@ -1,4 +1,5 @@
 package Seminar3.Task5;
+
 //1. Напишите метод,на вход которогоподаётся двумерный строковый массив
 //        размером 4х4. При подаче массива другого размера необходимо бросить
 //        исключение MyArraySizeException.
@@ -12,12 +13,12 @@ package Seminar3.Task5;
 //        результатрасчета (сумму элементов,при условиичто подали на вход
 //        корректныймассив).
 public class Program {
-    public static int sumMatrix(String[][] matr, int limit){
-        if (matr.length != limit){
+    public static int sumMatrix(String[][] matr, int limit) {
+        if (matr.length != limit) {
             throw new MyArraySizeException(); // если число вложенных массивов не равно параметру Limit
         } else {
             for (int i = 0; i < matr.length; i++) {
-                if (matr[i].length != limit){
+                if (matr[i].length != limit) {
                     throw new MyArraySizeException(); // если длина любого вложенного массива не равна параметру Limit
                 }
             }
@@ -27,8 +28,8 @@ public class Program {
             for (int j = 0; j < matr[1].length; j++) { //Integer.valueOf() and Integer.parseInt()
                 try {
                     sum += Integer.parseInt(matr[i][j]);
-                } catch(NumberFormatException e){
-                    throw new MyArrayDataException(); // Ловим ошибку парсинга строки в число и кидаем свою ошибку
+                } catch (NumberFormatException e) {
+                    throw new MyArrayDataException("Ошибка в ячейке " + "[" + i + "] " + "[" + j + "]"); // Ловим ошибку парсинга строки в число и кидаем свою ошибку
                 }
             }
         }
@@ -36,18 +37,43 @@ public class Program {
     }
 
     public static void main(String[] args) {
-        String [][] matrix = {
-                {"1","2","3","4"},
-                {"1","20","3","4"},
-                {"1","2","1","4"},
-                {"1","2","3","2"}
+        // Правильная матрица
+        String[][] matrix = {
+                {"1", "2", "3", "4"},
+                {"1", "20", "3", "4"},
+                {"1", "2", "3", "3"},
+                {"1", "2", "3", "4"}
         };
-        System.out.println(sumMatrix(matrix, 4));
+
+//        // Матрица с нечисловой ячейкой
+//        String[][] matrix = {
+//                {"1", "2", "3", "4"},
+//                {"1", "20", "ривет", "4"},
+//                {"1", "2", "ыы", "3"},
+//                {"1", "2", "3", "4"}
+//        };
+//        // Массив, не являющийся матрицей.
+//        String[][] matrix = {
+//                {"1", "2", "3", "4", "4"},
+//                {"1", "20", "3", "4"},
+//                {"1", "2", "3", "3", "3"},
+//                {"1", "2", "3", "4"}
+//        };
+        try {
+            System.out.println("Сумма элементов переданной матрицы равна " +
+                    sumMatrix(matrix, 4));
+        } catch (MyArraySizeException e) {
+            e.printStackTrace();
+        } catch (MyArrayDataException e){
+            e.printStackTrace();
+        }
+        System.out.println("Конец программы");
     }
 }
 
-class MyArraySizeException extends RuntimeException{
-    private String detailedMessage = "Неверный размер массива";
+class MyArraySizeException extends RuntimeException {
+    private String detailedMessage = "Неверный размер массива.";
+
     @Override
     public String getMessage() {
         return detailedMessage;
@@ -57,13 +83,18 @@ class MyArraySizeException extends RuntimeException{
     }
 }
 
-class MyArrayDataException extends RuntimeException{
-    private String detailedMessage = "Элемент не является целым числом";
+class MyArrayDataException extends RuntimeException {
+    private String detailedMessage = "Элемент не является целым числом.";
+
     @Override
     public String getMessage() {
         return detailedMessage;
     }
 
     public MyArrayDataException() {
+    }
+
+    public MyArrayDataException(String message) {
+        this.detailedMessage += " " + message;
     }
 }
